594d03332f6933b94a4b3a813d6736e560c25773
Vikas Bhat <vikas.bhat@oneadvanced.com>
2022-02-21 17:22:21 +0530

    Add project files.

---
+++ CSharpFundas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CSharpFundas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello Vikas!");
+
+        }
+    }
+}
